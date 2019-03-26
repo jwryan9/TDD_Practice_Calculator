c93e49541dc2f9ab9bb6181d90cec76856062a78
Jason Ryan <jason-ryan@uiowa.edu>
2019-03-26 17:08:32 -0500

    Passes tests for 0, 1, or 2 values.

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
     public static int Add(String numbers) {
-        int result;
+        int result = 0;
+
         if (numbers.equals("")) {
             result = 0;
         } else {
-            result = -1;
+            List<String> numbersStringList = Arrays.asList(numbers.split(","));
+            List<Integer> numbersIntList = new ArrayList<>();
+
+            for (String s : numbersStringList) numbersIntList.add(Integer.valueOf(s));
+
+            for (Integer i : numbersIntList) result += i;
         }
 
         return result;
